670e5912c6f4d677d87b5bca73fe6d2407f0f244
Christian Bulitta <github@bulitta.com>
2015-11-04 17:34:44 +0100

    added eclipse

--- 04_implementation/scripts/setup-ubuntu.sh
+++ 04_implementation/scripts/setup-ubuntu.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 
 
 run apt-get update
-run apt-get -y install git-cola git-gui awscli python-setuptools  synaptic geany curl gitk
+run apt-get -y install git-cola git-gui awscli python-setuptools  synaptic geany curl gitk eclipse
 run apt-get -y remove python-pip python-pip-whl python-virtualenv
 run easy_install pip
 run pip install virtualenv --upgrade
