9f199c9b9527bf5074fe9ed413068b8c7a53691a
Christian Bulitta <github@bulitta.com>
2015-11-09 18:31:28 +0100

    use argument (slave index)

--- 04_implementation/scripts/ssh-into-dcos-slave.sh
+++ 04_implementation/scripts/ssh-into-dcos-slave.sh
@@ -9,10 +9,12 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+SLAVE_INDEX="$1"
+
 # taken from https://docs.mesosphere.com/services/sshcluster/
 
-SLAVE_ID=`getSlaveAttribute.sh ${CURRENT_NODE_INFO_FILE} 0 "id"`
-SLAVE_INTERNAL_IP=`getSlaveAttribute.sh ${CURRENT_NODE_INFO_FILE} 0 "hostname"`
+SLAVE_ID=`getSlaveAttribute.sh ${CURRENT_NODE_INFO_FILE} ${SLAVE_INDEX} "id"`
+SLAVE_INTERNAL_IP=`getSlaveAttribute.sh ${CURRENT_NODE_INFO_FILE} ${SLAVE_INDEX} "hostname"`
 echo "SSH into SLAVE ID ${SLAVE_ID} with internal IP-Address ${SLAVE_INTERNAL_IP}..."
 
 dcos node ssh --master-proxy --slave=${SLAVE_ID}
