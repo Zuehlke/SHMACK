cb168fa41bde18656f773748a27eea2405a878f4
Christian Bulitta <github@bulitta.com>
2015-11-10 18:27:54 +0100

    added "run-on-dcos-master.sh"

---
+++ 04_implementation/scripts/run-on-dcos-master.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+cd `dirname ${BASH_SOURCE[0]}`
+. ./shmack_env
+
+if [ "$#" -gt 1 ]; then
+    echo "Missing arguments"
+    echo "Usage: `basename $0` <commands>"
+    exit 1
+fi
+
+MASTER_IP_ADDRESS=`cat "${CURRENT_MASTER_NODE_SSH_IP_ADDRESS_FILE}"`
+ssh -i ${SSH_KEY_LOCATION} core@${MASTER_IP_ADDRESS} "$@"
+
