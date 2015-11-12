856614b5bcf541d063d423f70ed39a2be76c884b
Christian Bulitta <github@bulitta.com>
2015-11-12 12:32:31 +0100

    +# we do NOT want to use "dcos node ssh --master-proxy --slave=${SLAVE_ID}" as we cannot reliably determine which slave Adress or IP is used.

--- 04_implementation/scripts/ssh-into-dcos-slave.sh
+++ 04_implementation/scripts/ssh-into-dcos-slave.sh
@@ -13,9 +13,13 @@ SLAVE_INDEX="$1"
 
 # taken from https://docs.mesosphere.com/services/sshcluster/
 
+MASTER_IP_ADDRESS=`cat "${CURRENT_MASTER_NODE_SSH_IP_ADDRESS_FILE}"`
 SLAVE_ID=`getSlaveAttribute.sh ${CURRENT_NODE_INFO_FILE} ${SLAVE_INDEX} "id"`
 SLAVE_INTERNAL_IP=`getSlaveAttribute.sh ${CURRENT_NODE_INFO_FILE} ${SLAVE_INDEX} "hostname"`
 echo "SSH into SLAVE ID ${SLAVE_ID} with internal IP-Address ${SLAVE_INTERNAL_IP}..."
 
-dcos node ssh --master-proxy --slave=${SLAVE_ID}
+ssh -A -t -i ${SSH_KEY_LOCATION} core@${MASTER_IP_ADDRESS} ssh -A -t core@${SLAVE_INTERNAL_IP}
+
+# we do NOT want to use "dcos node ssh --master-proxy --slave=${SLAVE_ID}" as we cannot reliably determine which slave Adress or IP is used.
+# to know this is important for sync-to-dcos-master-and-slave.sh to work without a new authentification.
 
