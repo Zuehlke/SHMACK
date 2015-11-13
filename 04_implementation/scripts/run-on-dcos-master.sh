5ce385584063f7ecd1ae4964ea81d653e855f905
Christian Bulitta <github@bulitta.com>
2015-11-13 11:34:41 +0100

    need to source /etc/profile.d/dcos.sh to have JAVA_HOME and PATH for hadoop

--- 04_implementation/scripts/run-on-dcos-master.sh
+++ 04_implementation/scripts/run-on-dcos-master.sh
@@ -3,12 +3,13 @@
 cd `dirname ${BASH_SOURCE[0]}`
 . ./shmack_env
 
-if [ "$#" -gt 1 ]; then
+if [ "$#" -lt 1 ]; then
     echo "Missing arguments"
     echo "Usage: `basename $0` <commands>"
     exit 1
 fi
 
 MASTER_IP_ADDRESS=`cat "${CURRENT_MASTER_NODE_SSH_IP_ADDRESS_FILE}"`
-ssh -i ${SSH_KEY_LOCATION} core@${MASTER_IP_ADDRESS} "$@"
+echo ssh -i ${SSH_KEY_LOCATION} core@${MASTER_IP_ADDRESS} "$@"
+ssh -i ${SSH_KEY_LOCATION} core@${MASTER_IP_ADDRESS} "source /etc/profile.d/dcos.sh ; "$@" "
 
