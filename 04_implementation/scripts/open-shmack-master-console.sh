d93e46ce07b6199c48a523e9352ebc7a0b91633a
Christian Bulitta <github@bulitta.com>
2015-11-17 12:21:21 +0100

    script to open browser with spark ui

--- 04_implementation/scripts/open-shmack-master-console.sh
+++ 04_implementation/scripts/open-shmack-master-console.sh
@@ -3,5 +3,4 @@
 cd `dirname ${BASH_SOURCE[0]}`
 . ./shmack_env
 
-xdg-open "http://`cat ${CURRENT_MESOS_MASTER_DNS_FILE}`" > /dev/null 2>&1 &
-echo "Done."
+open-browser.sh "http://`cat ${CURRENT_MESOS_MASTER_DNS_FILE}`" 
