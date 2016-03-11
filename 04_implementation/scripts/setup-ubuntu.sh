7575f7a0fb8f06b10346f39a426861e155ad2df9
hensb <sb.hendrik@gmail.com>
2016-03-11 14:31:23 +0000

    remove gradle from installation script

--- 04_implementation/scripts/setup-ubuntu.sh
+++ 04_implementation/scripts/setup-ubuntu.sh
@@ -4,7 +4,7 @@ cd `dirname ${BASH_SOURCE[0]}`
 . ./shmack_env
 
 run apt-get update
-run apt-get -y install git-gui awscli python-setuptools curl wget gradle openjdk-8-jdk scala scala-library apt-transport-https ca-certificates
+run apt-get -y install git-gui awscli python-setuptools curl wget openjdk-8-jdk scala scala-library apt-transport-https ca-certificates
 ## On Ubuntu 14.04 LTS, it was necessary to remove some Python libs and replace them with pip. 
 #run apt-get -y remove python-pip python-pip-whl python-virtualenv
 run easy_install pip
