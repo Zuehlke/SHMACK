833745dddabe0eabb759f21bda20bc860b4e3a15
Michael Springmann <michael.springmann@zuehlke.com>
2016-03-17 16:46:07 +0100

    [issue #14] Follwoup: Hide curl progress when opening zeppelin

--- 04_implementation/scripts/open-shmack-zeppelin.sh
+++ 04_implementation/scripts/open-shmack-zeppelin.sh
@@ -5,7 +5,7 @@ cd `dirname ${BASH_SOURCE[0]}`
 
 HAPROXY_STATS_FILE="${TMP_OUTPUT_DIR}/HaProxyStats.html"
 
-curl http://`cat ${CURRENT_PUBLIC_SLAVE_DNS_NAME_FILE}`:9090/haproxy?stats > ${HAPROXY_STATS_FILE}
+curl --silent "http://`cat ${CURRENT_PUBLIC_SLAVE_DNS_NAME_FILE}`:9090/haproxy?stats" > ${HAPROXY_STATS_FILE}
 
 ZEPPELIN_PORT=`cat ${HAPROXY_STATS_FILE} | grep --perl-regexp --max-count 1 --only-matching ">zeppelin_[0123456789]{2,5}<" | sed s/">zeppelin_"// | sed s/"<"//`
 
