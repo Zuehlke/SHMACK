14ef3ce2a8a7079765998e07f4b3b1032ae396c1
Michael Springmann <michael.springmann@zuehlke.com>
2016-05-11 08:01:32 +0200

    Updated code to make use of dcos node command rather than creating ssh command ourselves.

--- 04_implementation/scripts/ssh-into-dcos-slave.sh
+++ 04_implementation/scripts/ssh-into-dcos-slave.sh
@@ -11,19 +11,5 @@ fi
 
 SLAVE_INDEX="$1"
 
-# taken from https://docs.mesosphere.com/services/sshcluster/
-
-MASTER_IP_ADDRESS=`cat "${CURRENT_MASTER_NODE_SSH_IP_ADDRESS_FILE}"`
-SLAVE_ID=`getSlaveAttribute.sh ${CURRENT_NODE_INFO_FILE} ${SLAVE_INDEX} "id"`
-SLAVE_INTERNAL_IP=`getSlaveAttribute.sh ${CURRENT_NODE_INFO_FILE} ${SLAVE_INDEX} "hostname"`
-echo "SSH into SLAVE ID ${SLAVE_ID} with internal IP-Address ${SLAVE_INTERNAL_IP}..."
-echo "  (If this blocks, make sure the network does allow ssh. Most Corporate Networks don't!)"
-
-## according to https://docs.mesosphere.com/administration/sshcluster/ you could use dcos node ssh also for that
-# dcos node ssh --master-proxy --slave=${SLAVE_ID}
-## but the SLAVE_ID is not just an index, but the true ID ... and there's not yet an easy way to determine that :-( 
-ssh -A -t -i ${SSH_KEY_LOCATION} core@${MASTER_IP_ADDRESS} ssh -A -t core@${SLAVE_INTERNAL_IP}
-
-# we do NOT want to use "dcos node ssh --master-proxy --slave=${SLAVE_ID}" as we cannot reliably determine which slave address or IP is used.
-# to know this is important for sync-to-dcos-master-and-slave.sh to work without a new authentification.
-# see also https://mesosphere.com/blog/2015/07/22/new-dcos-cli-commands-for-logs-and-ssh/
+SLAVE_MESOS_ID=`dcos node | grep --only-matching --word-regexp "[0123456789abcdef-]*S${SLAVE_INDEX}"`
+run dcos node ssh --master-proxy --mesos-id=${SLAVE_MESOS_ID}
