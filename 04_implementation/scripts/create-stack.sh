afe33226ace6b9e2e7f83c87510db9895c3fdb04
Michael Springmann <michael.springmann@zuehlke.com>
2016-08-15 17:57:20 +0200

    Made it nicer to adjust instance types and make use of spot prices.

--- 04_implementation/scripts/create-stack.sh
+++ 04_implementation/scripts/create-stack.sh
@@ -19,6 +19,13 @@ cd `dirname ${BASH_SOURCE[0]}`
 # (https://aws.amazon.com/ec2/instance-types/) instead of cranking up generic instance count.
 SLAVE_INSTANCE_COUNT=5
 
+# Instance type as defined in https://aws.amazon.com/ec2/pricing/ used for all worker nodes / slaves
+# Default is m3.xlarge, which should remain the lowest setting.
+# Other options could be m3.2xlarge with same number of nodes but a bit more "beefy", 
+# or the corresponding c3 or r3 instances, depending if you need more computing power (c3) or memory (r3).
+SLAVE_INSTANCE_TYPE="m3.xlarge"
+#SLAVE_INSTANCE_TYPE="r3.2xlarge"
+
 # Space-separated list of packages to install (without asking)
 # 'hdfs' and 'spark' are needed to run the spark unit tests.
 # You can later install additional packages using 'dcos package install'
@@ -27,7 +34,7 @@ INSTALL_PACKAGES="hdfs spark chronos marathon marathon-lb"
 
 # Space-separated list of optional packages to install if user confirms with 'yes' 
 # You can later install additional packages using 'dcos package install'
-OPTIONAL_PACKAGES="cassandra"
+OPTIONAL_PACKAGES=""
 
 # Space-separated list of apps to install (without asking)
 # 'zeppelin' provides a nice UI for getting started - requires packages 'marathon' and 'marathon-lb', 
@@ -43,13 +50,18 @@ INSTALL_APPS=""
 run mkdir --parents ${TMP_OUTPUT_DIR}
 run mkdir --parents ${CURRENT_STATE_DIR}
 
-TEMPLATE_PARAMETERS="${TEMPLATE_PARAMETERS} ParameterKey=SlaveInstanceCount,ParameterValue=${SLAVE_INSTANCE_COUNT}"
+TEMPLATE_PARAMETERS="${TEMPLATE_PARAMETERS} ParameterKey=SlaveInstanceCount,ParameterValue=${SLAVE_INSTANCE_COUNT} "
+
+if [ "${OFFICIAL_TEMPLATE_URL}Unchanged" != "${TEMPLATE_URL}Unchanged" ] 
+	then 
+		TEMPLATE_PARAMETERS="${TEMPLATE_PARAMETERS} ParameterKey=MasterInstanceType,ParameterValue=${SLAVE_INSTANCE_TYPE} ParameterKey=SlaveInstanceType,ParameterValue=${SLAVE_INSTANCE_TYPE}  ParameterKey=PublicSlaveInstanceType,ParameterValue=${SLAVE_INSTANCE_TYPE} ParameterKey=SpotInstancePrice,ParameterValue=${MAX_SPOT_INSTANCE_PRICE} "
+fi
 
 CLOUD_FORMATION_OUTPUT_FILE=${TMP_OUTPUT_DIR}/cloud-formation-result.json
 
 echo "${STACK_NAME}" > ${CURRENT_STACK_NAME_FILE}
 
-echo "Creating stack ${STACK_NAME} in aws..."
+echo "Creating stack ${STACK_NAME} in aws... with ${TEMPLATE_PARAMETERS}"
 run aws cloudformation create-stack --output json --stack-name ${STACK_NAME} --template-url ${TEMPLATE_URL} --parameters ${TEMPLATE_PARAMETERS} --capabilities CAPABILITY_IAM | tee ${CLOUD_FORMATION_OUTPUT_FILE}
 
 echo "Extracting StackId:"
@@ -92,5 +104,8 @@ echo ${INSTALL_PACKAGES} > ${CURRENT_STACK_INSTALL_PACKAGES_FILE}
 echo ${OPTIONAL_PACKAGES} > ${CURRENT_STACK_OPTIONAL_PACKAGES_FILE}
 echo ${INSTALL_APPS} > ${CURRENT_STACK_INSTALL_APPS_FILE}
 echo ${OPTIONAL_APPS} > ${CURRENT_STACK_OPTIONAL_APPS_FILE}
+
+# Make sure authentication is current, so we won't run into issues when initializing the stack
+run dcos auth login
 
 run init-dcos-stack.sh
