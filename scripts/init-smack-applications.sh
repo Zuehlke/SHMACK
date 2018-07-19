1340df84ddb7c3e381a1d43a09f6864a96220f8a
Benedikt Wedenik <benedikt.wedenik@gmail.com>
2018-07-19 14:16:31 +0200

    Updated SMACK init scripts

--- scripts/init-smack-applications.sh
+++ scripts/init-smack-applications.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
 
-# Kafka Topics
+####################
+### Kafka Topics ###
+####################
 dcos kafka topic create sensor-reading --partitions 5 --replication 2
+dcos kafka topic create sensor-reading-accelerometer --partitions 5 --replication 2
 dcos kafka topic create data-analytics --partitions 5 --replication 2
+dcos kafka topic create dump --partitions 5 --replication 2
 
-# Hackzurich Sensor-ingestion app
+
+
+#######################################
+### Hackzurich Sensor-ingestion app ###
+#######################################
 dcos marathon app add /home/bewe/shmack/hackzurich-sensordataanalysis/sensor-ingestion/sensor-ingestion-options.json
 
-# KafkaToCassandra spark job
-dcos spark run --submit-args="--properties-file=service-configs/spark-hackzurich.KafkaToCassandra.conf --conf spark.cores.max=2 --driver-memory 4G --executor-memory 4G --class com.zuehlke.hackzurich.KafkaToCassandra https://s3-us-west-1.amazonaws.com/benedikt.wedenik.kafka.cassandra/KafkaToCassandra-all.jar"
 
-# Spark DataAnalytics spark job
-dcos spark run --submit-args="--properties-file=service-configs/spark-hackzurich.DataAnalytics.conf --conf spark.cores.max=2 --driver-memory 2G --conf spark.driver.cores=2 --executor-memory 1G --conf spark.executor.cores=1 --class com.zuehlke.hackzurich.DataAnalytics https://s3-us-west-1.amazonaws.com/benedikt.wedenik.kafka.cassandra/spark-data-analytics-all.jar"
 
-# Akka app to display data from Spark DataAnalytics
+##################################
+### KafkaToCassandra spark job ###
+##################################
+#dcos spark run --submit-args="--properties-file=service-configs/spark-hackzurich.KafkaToCassandra.conf --conf spark.cores.max=2 --driver-memory 8G --conf spark.driver.cores=2 --executor-memory 4G --conf spark.executor.cores=2 --class com.zuehlke.hackzurich.KafkaToCassandra https://s3-us-west-1.amazonaws.com/benedikt.wedenik.kafka.cassandra/KafkaToCassandra-all.jar"
+
+
+
+
+######################################
+### KafkaToAccelerometer spark job ###
+######################################
+dcos spark run --submit-args="--properties-file=service-configs/spark-hackzurich.KafkaToAccelerometer.conf --conf spark.cores.max=2 --driver-memory 8G --conf spark.driver.cores=2 --executor-memory 4G --conf spark.executor.cores=2 --class com.zuehlke.hackzurich.KafkaToAccelerometer https://s3-us-west-1.amazonaws.com/benedikt.wedenik.kafka.cassandra/KafkaToAccelerometer-all.jar"
+
+
+
+#####################################
+### Spark DataAnalytics spark job ###
+#####################################
+#dcos spark run --submit-args="--properties-file=service-configs/spark-hackzurich.DataAnalytics.conf --conf spark.cores.max=1 --driver-memory 2G --conf spark.driver.cores=1 --executor-memory 1G --conf spark.executor.cores=1 --class com.zuehlke.hackzurich.DataAnalytics https://s3-us-west-1.amazonaws.com/benedikt.wedenik.kafka.cassandra/spark-data-analytics-all.jar"
+dcos spark run --submit-args="--properties-file=service-configs/spark-hackzurich.DataAnalytics.conf --conf spark.cores.max=2 --driver-memory 8G --conf spark.driver.cores=2 --executor-memory 4G --conf spark.executor.cores=2 --class com.zuehlke.hackzurich.DataAnalytics https://s3-us-west-1.amazonaws.com/benedikt.wedenik.kafka.cassandra/spark-data-analytics-all.jar"
+
+
+
+#########################################################
+### Akka app to display data from Spark DataAnalytics ###
+#########################################################
 dcos marathon app add /home/bewe/shmack/hackzurich-sensordataanalysis/akka-data-analytics/akka-data-analytics-options.json
 
--- scripts/init-smack-stack.sh
+++ scripts/init-smack-stack.sh
@@ -7,7 +7,8 @@ sleep 40
 
 # dcos package install marathon --yes
 dcos package install marathon-lb --yes
-dcos package install spark --yes --options=service-configs/spark.json
+dcos package install spark --yes --options=service-configs/spark.json --package-version=2.1.0-2.2.1-1
+#dcos package install spark --yes --options=service-configs/spark_new.json --package-version=2.3.0-2.2.1-2
 dcos package install kafka --yes --package-version=1.1.19.1-0.10.1.0 --options=service-configs/kafka.json
 dcos package install cassandra --yes
 #dcos package install zeppelin --package-version=0.6.0 --yes
