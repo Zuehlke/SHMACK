61af52b16f3753f457d142113f8f0ffd8130a3ad
Benedikt Wedenik <benedikt.wedenik@gmail.com>
2018-07-25 20:46:04 +0200

    Init script: Cassandra version set to 2.1.0-3.0.16

--- scripts/init-smack-stack.sh
+++ scripts/init-smack-stack.sh
@@ -10,6 +10,6 @@ dcos package install marathon-lb --yes
 dcos package install spark --yes --options=service-configs/spark.json --package-version=2.1.0-2.2.1-1
 #dcos package install spark --yes --options=service-configs/spark_new.json --package-version=2.3.0-2.2.1-2
 dcos package install kafka --yes --package-version=1.1.19.1-0.10.1.0 --options=service-configs/kafka.json
-dcos package install cassandra --yes
+dcos package install cassandra --yes --package-version=2.1.0-3.0.16
 #dcos package install zeppelin --package-version=0.6.0 --yes
 
